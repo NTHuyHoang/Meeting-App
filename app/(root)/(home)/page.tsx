6d4727c709a361d192835ec3b684e877ca780c2e
NT Huy Hoang <lanhlung15122002@gmail.com>
2024-07-27 18:15:25 +0700

    commit time

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -4,7 +4,7 @@ import MeetingTypeList from "@/components/MeetingTypeList";
 const Home = () => {
     const now = new Date();
 
-    const time = now.toLocaleTimeString('it-IT', {hour: '2-digit', minute:
+    const time = now.toLocaleTimeString('en-US', {hour: '2-digit', minute:
     '2-digit'});
     const date = (new Intl.DateTimeFormat('en-US', {
         dateStyle: 'full'})).format(now);
